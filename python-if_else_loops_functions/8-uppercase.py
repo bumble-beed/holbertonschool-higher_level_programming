6e5f134886e631886fea11d79197fa5bc3b51964
bumble-beed <shirleen.workstuff@gmail.com>
2026-05-12 09:39:52 +0000

    uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-def isupper(c):
-    return ord(c) >= 65 and ord(c) <= 90
+def uppercase(str):
+    for i in str:
+        c = chr(ord(i) - 32)
+        return ord(str) >= 65 and ord(c) <= 90
