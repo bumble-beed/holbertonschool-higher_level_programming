7b7288cdf8fcf43ef91ad56e429a374a5245c3fb
bumble-beed <shirleen.workstuff@gmail.com>
2026-05-12 08:11:34 +0000

    add if condtion

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -2,4 +2,7 @@
 
 # print numbers 9 to 99 with , separating them and 2 decimal places
 for i in range(0, 100):
-    print("{:02d}".format(i))
+    if i == 99:
+        print("{:02d}".format(i))
+    else:
+        print("{:02d}".format(i), end=", ")
