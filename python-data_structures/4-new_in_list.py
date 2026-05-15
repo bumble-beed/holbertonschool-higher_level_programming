16346f11283575765d5700b7f08ff28fbe001515
bumble-beed <shirleen.workstuff@gmail.com>
2026-05-15 04:32:27 +0000

    corrections

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    new_list = my_list.copy()
-
+    # replace element in list at a specific position without modifying original list (i.e make a new list)
     if idx < 0 or idx >= len(my_list):
-        return my_list
-
+        return my_list[:]
+    new_list = my_list[:]
     new_list(idx) = element
-    return  new_list
+    return new_list
